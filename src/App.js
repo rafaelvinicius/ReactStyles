22579351a841dc0d39b5deea938a437b0a3fbfad
Rafael Vinicius <rafaelvinicius@rafaelvinicius.local>
2021-02-12 11:50:50 -0300

    Primeiro envio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { Button, Titulo, Texto, ImgTest, ImgTest2, BgDiv } from './Estilos'
 
 function App() {
+
+
+  const [UrlImg, setUlrImg] = useState("");
+  const [Loading, setLoading] = useState("");
+
+
+
+  async function PegaImagem(){
+
+    setLoading(true);
+
+    const response = await fetch("https://api.thecatapi.com/v1/images/search");
+    const jsonResult = await response.json();
+    // console.log(jsonResult);
+
+    if(jsonResult !== null){
+      setLoading(false);
+      setUlrImg(jsonResult[0].url);
+    }
+
+  }
+
+  useEffect(async () => {
+
+    PegaImagem();
+
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+
+        {Loading == true ? 'Carregando' : null}
+      
+        <Titulo>Titulo <span>Span</span> </Titulo>
+        <Button onClick={()=>PegaImagem()}>xxxx</Button>
+        <Texto color={'#F0F'} fontsize={35}>xxx asdf asdf asdf asdf asdfa ds</Texto>
+        <ImgTest ImgProps={{ MaxWidth: 300 }} src={UrlImg} />
+        <ImgTest2 ImgProps={{ MaxWidth: 300 }} src={UrlImg} />
+        <BgDiv UrlImg={UrlImg} />
+      
+      </>
   );
 }
 
